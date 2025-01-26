06c600372bdb40dab98318d91d2497601f3a44fb
GINA <wlskrla7@gmail.com>
2025-01-27 03:11:00 +0900

    2 finish

---
+++ Chapter_02/ch2/src/FloatEx1.java
@@ -0,0 +1,13 @@
+class FloatEx1 {
+    public static void main(String[] args) {
+        float f = 9.1234567f;
+        float f2 = 1.23456789f;
+        double d = 9.1234567;
+        
+        System.out.printf("123456789012345678901234%n");
+        System.out.printf("%f%n", f);
+        System.out.printf("f = %24.20f%n", f);
+        System.out.printf("f = %24.20f%n", f2);
+        System.out.printf("d = %24.20f%n", d);
+    }     
+}
