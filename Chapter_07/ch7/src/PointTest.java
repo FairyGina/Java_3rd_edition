e0b57f4bac0424aeba9ac1060954f3234b5c7ac1
GINA <wlskrla7@gmail.com>
2025-03-11 08:16:30 +0900

    7 ing

---
+++ Chapter_07/ch7/src/PointTest.java
@@ -0,0 +1,29 @@
+// class PointTest{
+//     public static void main(String[] args) {
+//         Point3D p3 = new Point3D();
+//     }
+// }
+
+// class Point{
+//     int x = 10;
+//     int y = 20;
+    
+//     Point(int x, int y){
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
+
+// class Point3D extends Point{
+//     int z = 30;
+    
+//     Point3D(){
+//         this.x = 100;
+//         this.y = 200;
+//         this.z = 300;
+//     }
+    
+//     String getLocation(){
+//         return "x : " + x + ", y : " + y + ", z : " + z;
+//     }
+// }
