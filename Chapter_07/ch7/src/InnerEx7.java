b12c2ffff017d06aa115af949c53ccc8ac0327b9
GINA <wlskrla7@gmail.com>
2025-03-20 02:04:21 +0900

    7 finish

---
+++ Chapter_07/ch7/src/InnerEx7.java
@@ -0,0 +1,15 @@
+// import java.awt.*;
+// import java.awt.event.*;
+
+// class InnerEx7{
+//     public static void main(String[] args){
+//         Button b = new Button("Start");
+//         b.addActionListener(new ActionListener());
+//     }
+// }
+
+// class ActionListener implements java.awt.event.ActionListener{
+//     public void actionPerformed(ActionEvent e){
+//         System.out.println("ActionEvent occurred!!!");
+//     }
+// }
