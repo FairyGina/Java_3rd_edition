ccf05137f112d71150eb25789e0260434691634d
GINA <wlskrla7@gmail.com>
2025-03-20 03:13:14 +0900

    8 ing

---
+++ Chapter_08/ch8/src/ExceptionEx1.java
@@ -0,0 +1,9 @@
+// class ExceptionEx1{
+//     public static void main(String[] args){
+//         try{
+//             try{ } catch(Exception e){ }
+//         } catch(Exception e){
+//             try{ } catch(Exception e1){ }
+//         }
+//     } // main의 끝
+// }
