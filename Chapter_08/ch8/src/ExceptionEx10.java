3a274fc91b092e9048335c12860279cd0889e77f
GINA <wlskrla7@gmail.com>
2025-03-23 01:22:02 +0900

    8 ing

---
+++ Chapter_08/ch8/src/ExceptionEx10.java
@@ -0,0 +1,5 @@
+// class ExceptionEx10{
+//     public static void main(String[] args){
+//         throw new Exception(); // Exception을 강제로 발생시킨다.
+//     }
+// }
---
+++ Chapter_08/ch8/src/ExceptionEx14.java
@@ -0,0 +1,14 @@
+// class ExceptionEx14{
+//     public static void main(String[] args){
+//         try{
+//             method1();
+//         } catch(Exception e){
+//             System.out.println("An exception was processed in main method.");
+//             e.printStackTrace();
+//         }
+//     }
+
+//     static void method1() throws Exception{
+//         throw new Exception();
+//     }
+// }
